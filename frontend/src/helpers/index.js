78e47fa494086bcf6aee86b4da340d973fe1436a
Hayes Choy <hayes.choy@student.unsw.edu.au>
2023-07-02 00:16:10 +1000

    search slightly done

--- frontend/src/helpers/index.js
+++ frontend/src/helpers/index.js
@@ -2,5 +2,6 @@ import moment from "moment";
 
 // Takes in date object, returns string.
 export const addMinutes = (date, minutes) => {
-  return moment.utc(date).add(minutes, "minutes").format("YYYY-MM-DD h:mm a");
+  return moment.utc(date).add(minutes, "minutes");
+  // .format("YYYY-MM-DD h:mm a");
 };
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,21 +1,29 @@
-import { Container } from "@chakra-ui/react";
+import { Container, VStack, Text, Card, CardBody, Divider} from "@chakra-ui/react";
 import { JourneyTimeline } from "../components";
 
 const dummyData = {
   percent_chance: 50,
   departure_airport: "LAX", // AIRPORT CODE
-  departure_time_scheduled: "2023-07-01T12:07:37Z", // UTC
+  departure_time_scheduled: "2023-07-01T10:30:37Z", // UTC
   arrival_airport: "2023-07-01T09:07:37Z", // UTC
-  predicted_bag_check: 30, // FIXED amount of time for bag check in
-  predicted_security: 40, // in minutes
+  predicted_bag_check: 20, // FIXED amount of time for bag check in
+  predicted_security: 30, // in minutes
   predicted_flight_delay: 60, // in minutes
 };
 
 const Search = () => {
   return (
     <Container>
-      <div>SEARCH PAGE - 50% is the chance you make your flight.</div>
-      <JourneyTimeline likelihood={dummyData} />
+      <Card>
+        <CardBody>
+          <VStack>
+            <Text fontSize="6xl">50%</Text>
+            <Text>is the chance you will make your flight.</Text>
+            <Divider style={{marginTop: "50px", marginBottom: "50px" }} />
+            <JourneyTimeline likelihood={dummyData} />
+          </VStack>
+        </CardBody>
+      </Card>
     </Container>
   );
 };
